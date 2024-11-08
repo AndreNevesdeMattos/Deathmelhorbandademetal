466cc202bf5eb73fda490962832c34e481270684
AndreNevesdeMattos <00001129662299sp@al.educacao.sp.gov.br>
2024-11-08 09:47:59 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Metal',
+    'O que é o Death?',
+    'O Death é uma banda de death metal'
+)
+
+criaCartao(
+    'South Park',
+    'Qual o personagem que sempe morre?',
+    `É o Kenny'
+)
+
+criaCartao(
+    'Geogafia',
+    'Onde fica a Bolívia?',
+    'Fica na América do Sul'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
